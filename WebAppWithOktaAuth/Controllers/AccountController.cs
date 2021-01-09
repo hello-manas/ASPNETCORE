20c32009d46c7f614a0c50c3b81e782ae73ce48d
hello-manas <manasrdash@outlook.com>
2021-01-09 17:59:25 +0530

    WebApp with Okta Auth
    Web App with Okta Auth

---
+++ WebAppWithOktaAuth/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppWithOktaAuth.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if(!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult logout()
+        {
+            return new SignOutResult(new[]
+            {
+                OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            });
+        }
+    }
+}
